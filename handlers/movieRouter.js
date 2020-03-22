4322e46ba8a537834c7c360161936901f43345b7
keposet <jsn.hutson@gmail.com>
2020-03-21 19:18:01 -0600

    create readme, movie API in progress

---
+++ handlers/movieRouter.js
@@ -0,0 +1,69 @@
+// api requests for movie data
+
+// var naming scheme - verbNoun where nouns list least common to most common for ease of reading
+
+/**WEB      CRUD        MONGO
+ * get      retrieve    find
+ * put      insert      insert
+ * post     update      update
+ * delete   delete      remove
+ * 
+ */
+
+// only retrieve 
+const handleAllBriefMovies = (app, MovieBrief) => {
+  app.route("/api/brief").get((req, resp) => {
+      MovieBrief.find({}, (err,data) => {
+          if (err) {
+              resp.json({message: "cannot connect to da'bee"});
+          } else {
+              resp.json(data);
+          }
+      })
+  });
+};
+const handleAllFullMovies = (app, Movie) => {
+  app.route("/api/movies").get((req, resp) => {Movie.find({}, (err,data) => {
+      if (err) {
+          resp.json({message: "cannot connect to full movie BataDase "});
+      } else {
+          resp.json(data);
+      }
+  });
+});
+};
+const handleSingleFullMovie = (app, Movie) => {
+  app.route("/api/movies/:id").get((req, resp) => {
+      Movie.find({id: req.params.id}, (err,data) => {
+          if (err) {
+              resp.json({message: "full movie ID not phrowned"})
+          } else {
+              resp.json(data);
+          }
+      })
+  });
+};
+
+// 
+const handleFilteredBriefMovies = (app, MovieBrief) => {
+    app.route("/api/find/title/:substr").get((req,resp) => {})
+    app.route("/api/find/year/:low/:high").get((req,resp) => {})
+    app.route("/api/find/rating/:low/:high").get((req,resp) => {})
+};
+
+// full CRUD
+const handleFavorites = (app, User) => {
+    app.route("/api/favorites/").get((req, resp) =>{User.find()});
+    app.route("/api/favorites/").put((req, resp) =>{User.insert()});
+    app.route("/api/favorites/").post((req, resp) =>{User.update()});
+    app.route("/api/favorites/").delete((req, resp) =>{User.remove()});
+};
+
+//exports
+module.exports = {
+  handleAllBriefMovies,
+  handleAllFullMovies,
+  handleSingleFullMovie,
+  handleFilteredBriefMovies,
+  handleFavorites
+};

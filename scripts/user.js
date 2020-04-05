39650c3b2841ec9c8fb5aad300c88d403c852e19
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-04-04 19:24:11 -0600

    removed sensitive parameters

--- scripts/user.js
+++ scripts/user.js
@@ -2,7 +2,7 @@ const {checkAuthenticated} = require('./auth');
 const User = require('../db/userModel');
 
 function getUserProfile(req, resp) {
-    User.findOne({id: req.user}).exec((err, user)=>{
+    User.findOne({id: req.user}).select("-_id -password_bcrypt -apikey").exec((err, user)=>{
         if(err){
             resp.statusCode = 500;
             resp.json({msg: "Server error"})
@@ -20,4 +20,4 @@ const setupUser = (app)=>{
 
 module.exports = {
     setupUser
-}
+};

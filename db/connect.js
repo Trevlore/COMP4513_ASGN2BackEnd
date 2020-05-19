8ad5245e0b5b029aead5df73a21fb405a3cc50b4
Trevor Brown <trevor.j.a.brown@gmail.com>
2020-05-18 18:25:09 -0600

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 //mongoose.set('debug', true);
 
 const connect = () => {
-    const opt = {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        auth: {
-            user: process.env.MONGO_USER,
-            password: process.env.MONGO_PASSWORD
-        }
-    };
+//     const opt = {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true,
+//         auth: {
+//             user: process.env.MONGO_USER,
+//             password: process.env.MONGO_PASSWORD
+//         }
+//     };
     mongoose.connect(process.env.MONGO_URL, opt);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
